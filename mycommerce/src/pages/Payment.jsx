521f44ca775c8187075311276fd32ec5cc6523a6
alexsouza10 <alexsouza160298@gmail.com>
2023-10-21 18:27:29 -0300

    feat: payment implementation

--- mycommerce/src/pages/Payment.jsx
+++ mycommerce/src/pages/Payment.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from "react";
+import { getItem } from "../services/LocalStorageFuncs";
+import { AiFillCheckCircle } from 'react-icons/ai';
+import { PaymentStuyled } from '../styles/PaymentStyle'
 
 export const Payment = (props) => {
-  const { params: { price } } = props.match
+  const {
+    params: { price },
+  } = props.match;
+  const user = getItem("usuario");
   return (
-    <div>{price}</div>
-  )
-}
+    <div>
+      <h3>Sua compra foi concluída com sucesso!</h3>
+      <span><AiFillCheckCircle /></span>
+        <img src="" alt="" />
+        <p>{`Valor: R$ ${price}`}</p>
+        <p>{`Usuário: ${user.name}`}</p>
+        <p>{`Prazo: ${Math.ceil(Math.random() * 5) + 1} dias.`}</p>
+    </div>
+  );
+};
--- mycommerce/src/pages/ProfileEdit.jsx
+++ mycommerce/src/pages/ProfileEdit.jsx
@@ -2,64 +2,66 @@ import React, { useState } from "react";
 import { getItem, setItem } from "../services/LocalStorageFuncs";
 
 export const ProfileEdit = (props) => {
-  const user = getItem('usuario')
-  const [ name, setName ] = useState(user.name || '');
-  const [ email, setEmail ] = useState(user.email || '');
-  const [ pass, setPass ] = useState(user.pass || '');
-  const [ img, setImg ] = useState(user.img ||'');
-  const [ cpf, setCpf ] = useState(user.cpf || '');
+  const user = getItem('usuario');
+  const [name, setName] = useState(user.name || '');
+  const [email, setEmail] = useState(user.email || '');
+  const [pass, setPass] = useState(user.pass || '');
+  const [img, setImg] = useState(user.img || '');
+  const [cpf, setCpf] = useState(user.cpf || '');
 
   const cond = (
     name.length > 4 && (email.includes('@') && email.length > 8)
     && pass.length > 8 && img.length > 4 && cpf.length === 12
-  )
+  );
 
-const saveChanges = () => {
-  setItem('usuario',(name,email,pass,img,cpf))
-  const {history: { push }} = props;
-  push('/profile')
-}
+  const saveChanges = () => {
+    // Correção do uso de setItem
+    setItem('usuario', { name, email, pass, img, cpf });
+    const { history: { push } } = props;
+    push('/profile');
+  }
 
   return (
     <div>
       <p>Name</p>
-      <input 
-      type="text"
-      value={name}
-      onChange={({target:{value}}) =>setName(value) }
+      <input
+        type="text"
+        value={name}
+        onChange={({ target: { value } }) => setName(value)}
       />
-      
+
       <p>Email</p>
-      <input 
-      type="email" 
-      value={email}
-      onChange={({target:{value}}) => setEmail(value)}
+      <input
+        type="email"
+        value={email}
+        onChange={({ target: { value } }) => setEmail(value)}
       />
-      
+
       <p>Password</p>
-      <input 
-      type="password" 
-      value={pass}
-      onChange={({target:{value}}) => setPass(value)}
+      <input
+        type="password"
+        value={pass}
+        onChange={({ target: { value } }) => setPass(value)}
       />
-      
+
       <p>Image Url</p>
-      <input 
-      type="text" 
-      alue={img}
-      onChange={({target:{value}}) => setImg(value)}
+      <input
+        type="text"
+        value={img}
+        onChange={({ target: { value } }) => setImg(value)}
       />
-      
+
       <p>CPF</p>
-      <input 
-      type="number" 
-      alue={cpf}
-      onChange={({target:{value}}) => setCpf(value)}
+      <input
+        type="number"
+        value={cpf}
+        onChange={({ target: { value } }) => setCpf(value)}
       />
-      
+
       <br /> <br /> <br />
+      {/* Correção na propriedade disabled */}
       <button
-        disable={!cond}
+        disabled={!cond}
         onClick={saveChanges}
       >
         Save Changes
