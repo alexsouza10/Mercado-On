b124214970ac860c01fb6bf35033ac7bfbea9c2c
alexsouza10 <alexsouza160298@gmail.com>
2023-11-05 12:15:31 -0300

    feat: profileEdit

--- mycommerce/src/pages/ProfileEdit.jsx
+++ mycommerce/src/pages/ProfileEdit.jsx
@@ -1,25 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { getItem, setItem } from "../services/LocalStorageFuncs";
 
 export const ProfileEdit = (props) => {
-  const user = getItem('usuario');
-  const [name, setName] = useState(user?.name || '');
-  const [email, setEmail] = useState(user?.email || '');
-  const [pass, setPass] = useState(user?.pass || '');
-  const [img, setImg] = useState(user?.img || '');
-  const [cpf, setCpf] = useState(user?.cpf || '');
+  // Initialize states
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [img, setImg] = useState('');
+  const [cpf, setCpf] = useState('');
 
-  const cond = (
-    name.length > 4 && (email.includes('@') && email.length > 8)
-    && pass.length > 8 && img.length > 4 && cpf.length === 12
-  );
+  // Fetch user data once when the component mounts
+  useEffect(() => {
+    const user = getItem('usuario');
+    if (user) {
+      setName(user.name || '');
+      setEmail(user.email || '');
+      setPass(user.pass || '');
+      setImg(user.img || '');
+      setCpf(user.cpf || '');
+    }
+  }, []); 
+
+
+  const cond =
+    name.length > 4 &&
+    email.includes('@') &&
+    email.length > 8 &&
+    pass.length > 8 &&
+    img.length > 4 &&
+    String(cpf).length === 11; 
+
 
   const saveChanges = () => {
     const userData = { name, email, pass, img, cpf };
     setItem('usuario', userData);
     const { history: { push } } = props;
     push('/profile');
-  }
+  };
 
   return (
     <div>
@@ -29,7 +46,7 @@ export const ProfileEdit = (props) => {
         value={name}
         onChange={({ target: { value } }) => setName(value)}
       />
-      
+
       <p>Email</p>
       <input
         type="email"
@@ -46,9 +63,11 @@ export const ProfileEdit = (props) => {
 
       <p>Image Url</p>
       <input
-        type="text"
-        value={img}
-        onChange={({ target: { value } }) => setImg(value)}
+        type="file"
+        onChange={(e) => {
+          const file = e.target.files[0];
+          setImg(file); 
+        }}
       />
 
       <p>CPF</p>
@@ -59,13 +78,12 @@ export const ProfileEdit = (props) => {
       />
 
       <br /> <br /> <br />
-      {/* Correção na propriedade disabled */}
-      <button
-        disabled={!cond}
-        onClick={saveChanges}
-      >
+
+      <button disabled={!cond} onClick={saveChanges}>
         Save Changes
       </button>
     </div>
   );
 };
+
+
