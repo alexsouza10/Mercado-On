342111c6519a431de23b696afcb27931bddb5bb9
alexsouza10 <alexsouza160298@gmail.com>
2023-11-05 12:13:27 -0300

    feat: profile config

--- mycommerce/src/pages/Profile.jsx
+++ mycommerce/src/pages/Profile.jsx
@@ -4,18 +4,25 @@ import { getItem } from "../services/LocalStorageFuncs";
 export const Profile = (props) => {
   const user = getItem("usuario");
 
+  // Check if 'user' is null or undefined before accessing its properties
+  const name = user ? user.name : " ";
+  const cpf = user ? user.cpf : " ";
+  const email = user ? user.email : " ";
+  const imgSrc = user ? user.img : " "; // Handle 'null' or 'undefined' image
+
   const toProfileEdit = () => {
     const {
-      history: { push } } = props;
+      history: { push },
+    } = props;
     push("/profile/edit");
   };
 
   return (
     <div>
-      <p>{`Name: ${user.name}`}</p>
-      <p>{`CPF: ${user.cpf}`}</p>
-      <p>{`E-mail: ${user.email}`}</p>
-      <img src={user.img} alt="img-user" />
+      <p>{`Name: ${name}`}</p>
+      <p>{`CPF: ${cpf}`}</p>
+      <p>{`E-mail: ${email}`}</p>
+      {imgSrc && <img src={imgSrc} alt="img-user" />} {/* Display image only if 'imgSrc' exists */}
       <br /> <br />
       <button onClick={toProfileEdit}>Edit Profile</button>
     </div>
